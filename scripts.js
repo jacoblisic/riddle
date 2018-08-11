ce14b762e6a1cd9ece4e5e095cddf06692a23887
Jacob <jlisic@kent.edu>
2018-08-10 20:32:07 -0400

    Started developing the site. It is kinda pretty now.

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById("test").onclick = function() {clicked()};
+
+function clicked() {
+  document.getElementById("test").innerHTML = "This is the paragaph that appears when you click on the button.";
+}
